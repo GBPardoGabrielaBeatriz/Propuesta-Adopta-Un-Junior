d50987c53d97e484d39ab0807c8e404646fd3ba6
Gabriela <gabb.pardo@gmail.com>
2024-08-11 14:38:44 -0300

    Comenzamos con la demo

---
+++ src/main/java/com/AdoptaUnJunior/demo/AdoptaUnJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.AdoptaUnJunior.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaUnJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaUnJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AdoptaUnJunior/demo/AdoptaUnJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdoptaUnJunior.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaUnJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
